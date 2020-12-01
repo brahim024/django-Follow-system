97aea5dd38fe496047bfa17ba62f53e5257a27b9
brahim024 <ifninos168@gmail.com>
2020-12-01 15:21:24 +0100

    add test for unittest

--- functional_test.py
+++ functional_test.py
@@ -1,1 +1,13 @@
-functional_test
+from selenium import webdriver
+import unittest
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser=webdriver.Chrome(executable_path=r'C:\Users\acer\Downloads\chromedriver_win32\chromedriver.exe')
+	def tearDown(self):
+		self.browser.quit()
+	def test_can_star_a_list_and_retrieve_it_lates(self):
+		self.browser.get('http://localhost:8000')
+		self.assertin('Todo',self.browser.title)
+		self.fail('Finish the test!')
+if __name__=='__main__':
+	unittest.main(warnings='igore')
