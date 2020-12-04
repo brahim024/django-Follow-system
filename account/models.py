2255aef3a1417e66c5cc130b0338fce562f59853
brahim024 <ifninos168@gmail.com>
2020-12-04 17:07:44 +0100

    add model

--- account/models.py
+++ account/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.conf import settings
-
+rom django.contrib.auth import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
+
+
+class Contact(models.Model):
+	user_from=models.ForeignKey(User,ralated_name='rel_from_set',on_delete=models.CASCADE)
+	user_to=models.ForeignKey(User,ralated_name='rel_to_set',on_delete=models.CASCADE)
+	created=models.DateTimeField(auto_now=True,db_index=True)
+	class Meta:
+		ordering='-created'
+	def __str__(self):
+		return f'{self.user_from} follows {self.user_to}'
+		
--- project/settings.py
+++ project/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lists',
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
