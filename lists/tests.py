e0b903cbb7f2cda4203cb9bb7e7e8c141581115f
brahim024 <ifninos168@gmail.com>
2020-12-08 16:37:38 +0100

    add bad test

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
-from .views import index
+from list.views import index
 # Create your tests here.
-class HomePagetest(TestCase):
+'''class HomePagetest(TestCase):
 	def test_view_url_exists_at_desired_location(self):
     	response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+'''
+class SmokeTest(TestCase):
+	def test_bad(self):
+		self.assertEqual(1+1,2)
--- project/settings.py
+++ project/settings.py
@@ -33,14 +33,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lists',
-    'account',
     'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',t,
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
