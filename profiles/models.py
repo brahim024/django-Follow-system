df70766bad01517cf48ea476ad7647caeefa56bb
brahim024 <ifninos168@gmail.com>
2021-02-11 21:37:52 +0100

    add profile app and create prodile models

---
+++ profiles/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Profile(models.Model):
+	user=models.OneToOneField(User,on_delete=models.CASCADE)
+	following=models.ManyToManyField(User,related_name='following',blank=True)
+	bio=models.TextField(default='Empty...')
+	updated=models.DateTimeField(auto_now=True)
+	create=models.DateTimeField(auto_now_add=True)
+
+	def profile_posts(self):
+		pass
+
+	def __str__(self):
+		return str(slef.user.username)
--- project/settings.py
+++ project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'lists',
+    'profiles',
     'social_django',
     'django.contrib.admin',
     'django.contrib.auth',
