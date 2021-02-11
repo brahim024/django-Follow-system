3ce363a70bad5193d168a9ce220811851c701979
brahim024 <ifninos168@gmail.com>
2021-02-11 21:14:25 +0100

    add dome tests

--- lists/test/test.py
+++ lists/test/test.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from selenium import webdriver
 import unittest
-
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser=webdriver.Chrome(executable_path=r'C:\Users\acer\Downloads\chromedriver_win32\chromedriver.exe')
@@ -10,9 +9,8 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_star_a_list_and_retrieve_it_lates(self):
 		self.browser.get('http://localhost:8000')
 		#self.browser.implicitly_wait(3)
-		self.assertIn('Django',self.browser.title)
-		
+		self.asserting('Todo',self.browser.title)
+		print(self.fail('Finish the test!'))
 
 if __name__=='__main__':
 	unittest.main(warnings='ignore')
-	
