d2503c72883fa1848a8e9e296223270f29683bc1
brahim024 <ifninos168@gmail.com>
2021-02-12 12:40:58 +0100

    add gegeric list views and base templates

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic.detail import ListView,DetailView
+from .models import Profile
 # Create your views here.
+class ProfileListView(ListView):
+	model=Profile
+	templates_name='profile/main.html'
+	context_objects_name='profile'
+
+	def get_queryset(self):
+		profile=Profile.objects.all()exclude(user=self.request.user)
+		
