9778c7678b0c7f8fb4938dff40e3549f6a8c7839
brahim024 <ifninos168@gmail.com>
2021-02-12 14:25:18 +0100

    add profile folling boolien

--- profiles/views.py
+++ profiles/views.py
@@ -14,3 +14,20 @@ class ProfileListView(ListView):
 class ProfileDetail(DetailView):
 	model=Profile
 	templates_name='profiles/profile_detail'
+
+	def get_object(self,**kwargs):
+		pk=self.kwargs.get('pk')
+		view_profile=Profile.objects.get(pk=pk)
+		return view_profile
+
+	def get_context_data(self,**kwargs):
+		context = super().get_context_data(**kwargs)
+		view_profile=self.get_object()
+		my_profile=Profile.objects.get(user=self.request.user)
+		if view_profile.user in my_profile.following.all():
+			follow=True
+		else:
+			follow=False
+
+		context['follow']=follow
+		return context
